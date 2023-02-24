9ed6af798983273533185a46f358ae7188422fb0
Robert Horn <rehorn09@gmail.com>
2023-02-23 21:04:50 -0600

    update some keymaps

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -1,8 +1,12 @@
 -- leader
-vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', {})
+vim.keymap.set('n', '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap('n', '<C-k>', ':wincmd k<cr>', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j<cr>', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h<cr>', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l<cr>', { silent = true })
+vim.keymap.set('n', '<C-k>', ':wincmd k<cr>', { silent = true })
+vim.keymap.set('n', '<C-j>', ':wincmd j<cr>', { silent = true })
+vim.keymap.set('n', '<C-h>', ':wincmd h<cr>', { silent = true })
+vim.keymap.set('n', '<C-l>', ':wincmd l<cr>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
