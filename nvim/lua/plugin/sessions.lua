776dfc91546832d97509906e50f778d599801e6a
Robert Horn <rehorn09@gmail.com>
2023-02-23 20:46:06 -0600

    ignore gitcommit for sessions

--- nvim/lua/plugin/sessions.lua
+++ nvim/lua/plugin/sessions.lua
@@ -2,7 +2,13 @@ return function ()
   vim.o.sessionoptions = 'buffers,curdir,folds,globals,tabpages,winpos,winsize'
 
   require('persisted').setup {
-    autoload = true
+    autoload = true,
+    should_autosave = function ()
+      if vim.bo.filetype == 'gitcommit' then
+        return false
+      end
+      return true
+    end
   }
 
   require('telescope').load_extension('persisted')
