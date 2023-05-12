7fa85126df3579397dd38ec99c10527ae2f95acc
Robert Horn <rehorn09@gmail.com>
2023-05-11 19:57:10 -0500

    update icon

--- nvim/lua/plugin/bufferline.lua
+++ nvim/lua/plugin/bufferline.lua
@@ -17,7 +17,7 @@ return function ()
         icon = "▎",
         style = "icon",
       },
-      buffer_close_icon = "",
+      buffer_close_icon = "",
       modified_icon = "●",
       close_icon = "",
       left_trunc_marker = "",
