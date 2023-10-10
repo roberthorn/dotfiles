7c994696563396642e01c7e533c3181d38920146
Robert Horn <rehorn09@gmail.com>
2023-10-09 20:41:23 -0500

    add rust lsp, fix gopls

--- nvim/lua/plugin/lspconfig.lua
+++ nvim/lua/plugin/lspconfig.lua
@@ -53,7 +53,20 @@ return function()
     },
     gopls = {
       on_attach = on_attach,
-      capabilites = capabilites
+      capabilites = capabilites,
+      root_dir = function(fname)
+        local util = require('lspconfig.util')
+        -- see: https://github.com/neovim/nvim-lspconfig/issues/804
+        -- see: https://github.com/neovim/nvim-lspconfig/issues/2733
+        local mod_cache = vim.trim(vim.fn.system 'go env GOMODCACHE')
+        if fname:sub(1, #mod_cache) == mod_cache then
+           local clients = vim.lsp.get_active_clients { name = 'gopls' }
+           if #clients > 0 then
+              return clients[#clients].config.root_dir
+           end
+        end
+        return util.root_pattern 'go.work'(fname) or util.root_pattern('go.mod', '.git')(fname)
+     end,
     },
     html = {
       on_attach = on_attach,
@@ -74,6 +87,10 @@ return function()
           }
         }
       }
+    },
+    rust_analyzer = {
+      capabilites = capabilites,
+      on_attach = on_attach,
     },
     terraformls = {
       capabilites = capabilites,
