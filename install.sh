e489f66daadbb1fa30cb926a4119a051940a21c6
Robert Horn <rehorn09@gmail.com>
2023-10-20 20:28:49 -0500

    update install

--- install.sh
+++ install.sh
@@ -15,21 +15,21 @@ p_is_linux() {
 
 # link config files
 if [[ ! -e ~/.zshrc ]]; then
-  ln -sf $REPOPATH/.zshrc $HOME/.zshrc
+  ln -sf $REPOPATH/.zshrc $ZDOTDIR/.zshrc
 fi
 if [[ ! -e ~/.zshenv ]]; then
-  ln -sf $REPOPATH/.zshenv $HOME/.zshenv
+  ln -sf $REPOPATH/.zshenv $ZDOTDIR/.zshenv
 fi
 if [[ ! -e ~/.gitconfig ]]; then
   ln -sf $REPOPATH/.gitconfig $HOME/.gitconfig
 fi
 if [[ ! -e ~/.tmux.conf ]]; then
-  ln -sf $REPOPATH/.tmux.conf $HOME/.tmux.conf
+  ln -sf $REPOPATH/.tmux.conf $XDG_CONFIG_HOME/tmux/.tmux.conf
 fi
 
 # neovim configuration
-rm -rf ~/.config/nvim
-cp -r ./nvim ~/.config/nvim
+rm -rf $XDG_CONFIG_HOME/nvim
+cp -r ./nvim $XDG_CONFIG_HOME/nvim
 
 # run :PackerSync
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
