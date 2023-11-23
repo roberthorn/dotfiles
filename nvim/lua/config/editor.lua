a94d0c3fe98efa81fb31b125d844ad9601d4554c
Robert Horn <rehorn09@gmail.com>
2023-11-23 15:12:23 -0600

    add harpoon, remove neoscroll

--- nvim/lua/config/editor.lua
+++ nvim/lua/config/editor.lua
@@ -220,6 +220,15 @@ return {
   },
 
   {
+    'ThePrimeagen/harpoon',
+    version = '*',
+    opts = {},
+    config = function ()
+      require('telescope').load_extension('harpoon')
+    end
+  },
+
+  {
     'ggandor/lightspeed.nvim',
     version = '*',
   },
@@ -246,19 +255,9 @@ return {
   },
 
   {
-    'karb94/neoscroll.nvim',
+    'declancm/cinnamon.nvim',
     version = '*',
-    opts = {
-      mappings = {'<C-u>', '<C-d>'},
-      hide_cursor = true,          -- Hide cursor while scrolling
-      stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-      use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-      respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-      easing_function = nil,        -- Default easing function
-      pre_hook = nil,              -- Function to run before the scrolling animation starts
-      post_hook = nil,
-    },
+    opts = {},
   },
 
   {
