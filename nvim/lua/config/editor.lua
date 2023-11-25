f535c3f4f319197199ca070b4761f07bdb839a8d
Robert Horn <rehorn09@gmail.com>
2023-11-24 19:30:18 -0600

    use conform

--- nvim/lua/config/editor.lua
+++ nvim/lua/config/editor.lua
@@ -217,6 +217,24 @@ return {
   },
 
   {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup {
+        formatters_by_ft = {
+          lua = { "stylua" },
+        },
+      }
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function(args)
+          require("conform").format { bufnr = args.buf }
+        end,
+      })
+    end,
+  },
+
+  {
     "ThePrimeagen/harpoon",
     version = "*",
     opts = {},
