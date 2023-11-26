1cf4dfe0ef635e8dfceef1bbf7045ab44e7807f0
Robert Horn <rehorn09@gmail.com>
2023-11-26 11:44:03 -0600

    disable more builtins

--- nvim/lua/rh/disable_builtin.lua
+++ nvim/lua/rh/disable_builtin.lua
@@ -1,1 +1,6 @@
-vim.g.loaded_tutor_mode_plugin = 1 -- disable vimtutor
+vim.g.loaded_tutor_mode_plugin = 1 -- disable "$VIMRUNTIME/plugin/tutor.vim
+vim.g.loaded_2html_plugin = 1 -- disable "$VIMRUNTIME/plugin/tohtml.vim
+vim.g.loaded_gzip = 1 -- disable "$VIMRUNTIME/plugin/gzip.vim
+vim.g.loaded_tarPlugin = 1 -- disable "$VIMRUNTIME/plugin/tarPlugin.vim
+vim.g.loaded_zipPlugin = 1 -- disable "$VIMRUNTIME/plugin/zipPlugin.vim
+vim.g.loaded_getscriptPlugin = 1 -- disable "$VIMRUNTIME/plugin/getscriptPlugin.vim
