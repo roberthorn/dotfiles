863d1acce5ea39b027dc65d5d31337a682644a0b
Robert Horn <rehorn09@gmail.com>
2023-11-25 20:20:41 -0600

    chore: more cleanup

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -237,29 +237,6 @@ return {
   },
 
   {
-    "ThePrimeagen/harpoon",
-    version = "*",
-    config = function()
-      local nmap = require("rh.keymap").nmap
-      require("telescope").load_extension "harpoon"
-
-      require("harpoon").setup {}
-
-      nmap { "<leader>hm", require("harpoon.mark").add_file }
-      nmap { "<leader>hl", require("harpoon.ui").toggle_quick_menu }
-
-      for i = 1, 5 do
-        nmap {
-          string.format("<leader>%s", i),
-          function()
-            require("harpoon.ui").nav_file(i)
-          end,
-        }
-      end
-    end,
-  },
-
-  {
     "ggandor/lightspeed.nvim",
     version = "*",
   },
