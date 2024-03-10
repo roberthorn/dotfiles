9418f7cad06591be9717c9d494b51efa4c796b8b
Robert Horn <rehorn09@gmail.com>
2024-03-10 13:30:33 -0500

    change tmux status line

--- zsh/lib/fzf.zsh
+++ zsh/lib/fzf.zsh
@@ -6,3 +6,9 @@ export FZF_DEFAULT_OPTS=" \
 --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+
+export FZF_CTRL_T_OPTS=" \
+--preview 'bat -n --color=always {}' \
+--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
