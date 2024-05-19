520b7aea45ce25a7c02f3dd489f6f87c93d553b4
Robert Horn <rehorn09@gmail.com>
2024-05-18 21:24:43 -0500

    chore: move disable builtin

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,9 +3,6 @@
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 
--- disable builtins
-require "rh.disable_builtin"
-
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
