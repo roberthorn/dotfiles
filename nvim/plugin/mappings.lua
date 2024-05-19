57445cf0b9707d77fa909b14d317da0414555ba3
Robert Horn <rehorn09@gmail.com>
2024-05-18 21:32:13 -0500

    refactor: use nmap in mappings config

--- nvim/plugin/mappings.lua
+++ nvim/plugin/mappings.lua
@@ -1,8 +1,9 @@
-vim.keymap.set("n", "<C-k>", ":wincmd k<cr>", { silent = true })
-vim.keymap.set("n", "<C-j>", ":wincmd j<cr>", { silent = true })
-vim.keymap.set("n", "<C-h>", ":wincmd h<cr>", { silent = true })
-vim.keymap.set("n", "<C-l>", ":wincmd l<cr>", { silent = true })
+local nmap = require("rh.keymap").nmap
+nmap { "<C-j>", ":wincmd j<cr>", { silent = true } }
+nmap { "<C-k>", ":wincmd k<cr>", { silent = true } }
+nmap { "<C-h>", ":wincmd h<cr>", { silent = true } }
+nmap { "<C-l>", ":wincmd l<cr>", { silent = true } }
 
 -- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+nmap { "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true } }
+nmap { "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true } }
