fce889a114dc1d2d322aa9ad4c7786d3111543f9
Robert Horn <rehorn09@gmail.com>
2024-06-07 08:26:29 -0500

    feat: alias ls for eza

--- zsh/lib/colors.zsh
+++ zsh/lib/colors.zsh
@@ -4,3 +4,5 @@ autoload -U colors && colors
 if [[ "$OSTYPE" == (darwin)* ]]; then
   ls -G . &>/dev/null && alias ls='ls -G'
 fi
+
+[[ -x "$(command -v eza)" ]] && alias ls='eza -l --group-directories-first --icons'
