96f1872f5af62fee06337483adae4410aeffeab2
Robert Horn <rehorn09@gmail.com>
2024-06-09 20:16:26 -0500

    feat: vim-tmux navigation

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -199,4 +199,22 @@ return {
     },
     config = true,
   },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
