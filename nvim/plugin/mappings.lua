ad8a210290161c2c8b076647e0f32e2b38cb527c
Robert Horn <rehorn09@gmail.com>
2024-08-22 18:02:02 -0500

    feat: dump vim-tmux-navigator

--- nvim/plugin/mappings.lua
+++ nvim/plugin/mappings.lua
@@ -1,8 +1,8 @@
 local nmap = require("rh.keymap").nmap
--- nmap { "<C-j>", ":wincmd j<cr>", { silent = true } }
--- nmap { "<C-k>", ":wincmd k<cr>", { silent = true } }
--- nmap { "<C-h>", ":wincmd h<cr>", { silent = true } }
--- nmap { "<C-l>", ":wincmd l<cr>", { silent = true } }
+nmap { "<C-j>", ":wincmd j<cr>", { silent = true } }
+nmap { "<C-k>", ":wincmd k<cr>", { silent = true } }
+nmap { "<C-h>", ":wincmd h<cr>", { silent = true } }
+nmap { "<C-l>", ":wincmd l<cr>", { silent = true } }
 
 -- Remap for dealing with word wrap
 nmap { "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true } }
