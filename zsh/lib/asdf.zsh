6183b82a84f1babed18d55a0c8466d66069d3b0b
Robert Horn <rehorn09@gmail.com>
2025-03-01 08:49:00 -0600

    feat: use asdf for managing go

--- zsh/lib/asdf.zsh
+++ zsh/lib/asdf.zsh
@@ -1,2 +1,5 @@
 export ASDF_DIRENV_BIN="/opt/homebrew/bin/direnv"
 eval "$($ASDF_DIRENV_BIN hook zsh)"
+
+# set GOROOT
+[[ -e ~/.asdf/plugins/golang/set-env.zsh ]] && source ~/.asdf/plugins/golang/set-env.zsh
--- zsh/lib/go.zsh
+++ zsh/lib/go.zsh
@@ -1,5 +1,4 @@
 export GOPATH=$HOME/Dev/go
-export GOROOT=/usr/local/go
 export GOBIN=$GOPATH/bin
 export GOCACHE=$XDG_CACHE_HOME/go-build
 export PATH=$PATH:$GOBIN:$GOROOT/bin
