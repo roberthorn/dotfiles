28314ea49ad53d62b91bb6ca85945ba3d0b1c05f
Robert Horn <rehorn09@gmail.com>
2026-04-06 13:49:49 -0400

    feat(nvim): add golang fmt via conform

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -104,6 +104,7 @@ return {
     config = function()
       require("conform").setup {
         formatters_by_ft = {
+          go = { "golangci-lint" },
           lua = { "stylua" },
           javascript = { "prettier" },
           jsx = { "prettier" },
